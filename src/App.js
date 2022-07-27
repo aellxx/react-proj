1d0be84702fe008720025a4fc68024e757466bd5
Ael <bluebayband@gmail.com>
2022-07-26 20:27:47 -0400

    To-do list update

--- src/App.js
+++ src/App.js
@@ -2,61 +2,54 @@ import Button from "./Button";
 import styles from "./App.module.css"
 import {useState, useEffect} from "react";
 
-function Hello() {
-  // function to run when component is destroyed
-  useEffect(() => {
-    console.log("hi")
-    // runs when component is destroyed
-    return () => console.log("bye");
-  }, []);
-
-  return <h1>hi</h1>
-}
-
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyWord] = useState("");
-
-  const onClick = () => {
-    // increment click count and update UI
-    setValue((prev) => prev+1);
-  }
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  
   const onChange = (event) => {
-    // saving the user input to the state "keyword"
-    setKeyWord(event.target.value);
+    // toDo = user input value 
+    setToDo(prev => event.target.value);
   }
 
-  // run every time counter changes
-  useEffect(
-    () => {console.log("COUNT:", counter);}, 
-    [counter]);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === ""){
+      return;
+    } 
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo(prev => "");
+  }
 
-  // run every time keyword changes
-  useEffect(
-    () => {
-      if (keyword !== "" && keyword.length > 5){
-        console.log("SEARCH FOR", keyword);
-      }
-    },
-    [keyword] 
-  );
-
-  const [showing, setShowing] = useState(false);
-  const onClick2 = () => setShowing(prev => !prev);
-
+  const done = (event) => {
+    event.preventDefault();
+    const jobDone = event.target.parentElement.id;
+    setToDos(currentArray => currentArray.filter(item => item !== jobDone));
+    console.log(toDos);
+  }
+  
   return (
     <div>
-      <input 
-        value={keyword} 
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search here..." 
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me!</button>
-
-      {showing ? <Hello /> : null /* if not showing, destroy component */} 
-      <button onClick={onClick2}>{showing ? "Hide": "Show"}</button>
+      <h1 className={styles.title}>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {
+          toDos.map((toDo, index) => 
+            <li key={index} id={toDo}>
+              <span>{toDo}</span>
+              <button onClick={done}>X</button>
+            </li>
+          )
+        }
+      </ul>
     </div>
   );
 }
