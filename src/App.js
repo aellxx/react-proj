579335a93f2198af35c4142e501d0a71f19f9b1c
Ael <bluebayband@gmail.com>
2022-07-26 20:33:15 -0400

    add comments & delete debug

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import styles from "./App.module.css"
 import {useState, useEffect} from "react";
 
 function App() {
+  // states for UI update
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
   
+  // Save user input value to "toDo"
   const onChange = (event) => {
-    // toDo = user input value 
     setToDo(prev => event.target.value);
   }
 
+  // Save new toDo to "toDos" state
   const onSubmit = (event) => {
     event.preventDefault();
     if (toDo === ""){
@@ -20,11 +22,14 @@ function App() {
     setToDo(prev => "");
   }
 
+  // remove a toDo when done
+  // x separate UI update needed
   const done = (event) => {
     event.preventDefault();
+    // get toDo from parent element
     const jobDone = event.target.parentElement.id;
+    // filter out
     setToDos(currentArray => currentArray.filter(item => item !== jobDone));
-    console.log(toDos);
   }
   
   return (
@@ -44,7 +49,7 @@ function App() {
         {
           toDos.map((toDo, index) => 
             <li key={index} id={toDo}>
-              <span>{toDo}</span>
+              <span>{toDo} </span>
               <button onClick={done}>X</button>
             </li>
           )
